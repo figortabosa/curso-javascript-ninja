3afcfe533654065c4fae0a593651320479ec8b6d
francisco igor tabosa dos santos <48896965+figortabosa@users.noreply.github.com>
2020-04-18 16:57:55 -0300

    Update challenge-08.js

--- challenge-08/challenge-08.js
+++ challenge-08/challenge-08.js
@@ -3,38 +3,43 @@ Declare uma variável chamada `sum` e atribua a ela uma função chamada
 `calculateSum`. A função deve receber dois parâmetros e retornar a soma
 desses parâmetros.
 */
-// ?
+/var sum = function calculateSum(x, y){
+	return 'A soma de '+ x +' e '+ y +' é igual a '+ (x + y) +'.';
+}
 
 /*
 Invoque a função criada acima, passando dois números que serão somados, e mostre
 o resultado no console, com a frase:
 "A soma de [VALOR 1] e [VALOR2] é igual a [RESULTADO]."
 */
-// ?
+var sum = function calculateSum(x, y){
+	return 'A soma de '+ x +' e '+ y +' é igual a '+ (x + y) +'.';
+}
 
 /*
 Mostre no console o nome da função criada acima, com a frase:
 "O nome da função que faz a soma é [NOME DA FUNÇÃO]."
 */
-// ?
+console.log("O nome da função que faz a soma é "+ sum.name +".");
 
 /*
 Crie uma função literal chamada `showName`. Essa função deve retornar o
 seu nome.
-*/
-// ?
+*/function showName(nome){
+	return nome;
+}
 
 /*
 Declare uma variável chamada `varShowName` que recebe a função criada acima.
 */
-// ?
+var varShowName = showName(nome);
 
 /*
 Usando a variável criada acima, mostre no console o nome e o retorno da função
 atribuída a ela, com a seguinte frase:
 "A função [NOME DA FUNÇÃO] retorna [RETORNO DA FUNÇÃO]."
 */
-// ?
+console.log("A função " + varShowName.name + " retorna "+ showName('igor') +".")
 
 /*
 Crie uma função literal chamada `calculator`, que funcione assim:
